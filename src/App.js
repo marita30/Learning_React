9828988d40a0971f719ba69034d6d439f388ea3b
marita30 <marita.jose.ch@gmail.com>
2019-05-06 15:52:03 -0600

    Agregando una carpeta llamada Person con un archivo llamado Person.js , agregando una const person retornando un parrafo luego modificamos App.js para mandar a imprimir el retunr de const al return de el imprimiendolo con una etiqueta de cierre <Person/>---- 32

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+// Agregando la function Person
+import Person from './Person/Person.js';
 
  class App extends Component {
    render(){
-     // return (
-     //   <div className="App">
-     //    <h1> Hi, I'm a React App </h1>
-     //   </div>
-     // );
+     return (
+       <div className="App">
+        <h1> Hi, I'm a React App </h1>
+        <p> This is really working!!</p>
+        <Person/>
+       </div>
+     );
 
-  return React.createElement('div',{ className: 'App'}, null, React.createElement('h1', null, 'Hi, I\'m React App') );
+  // return React.createElement('div',{ className: 'App'}, null, React.createElement('h1', null, 'Hi, I\'m React App') );
    }
 }
 
