7f6be01f4de9bfcf7ae079ec086cdbfad0c76f9f
marita30 <marita.jose.ch@gmail.com>
2019-05-07 14:13:09 -0600

    o de esta manera se puede hacer en el button---45

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ import Person from './Person/Person.js';
        <div className="App">
         <h1> Hi, I'm a React App </h1>
         <p> This is really working!!</p>
-        <button onClick={this.switchNameHandler.bind(this, 'M.J')}>Switch Name</button>
+        <button onClick={() =>this.switchNameHandler('M.J')}>Switch Name</button>
         <Person
          name={this.state.persons[0].name}
          age={this.state.persons[0].age} />
