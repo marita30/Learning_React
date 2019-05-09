d7ef8e1d277a246b3941b9bfc687a221f6d70e99
marita30 <marita.jose.ch@gmail.com>
2019-05-09 15:02:42 -0600

    Modificando el archivo App.js, agregando un stilo dinamico al botton agregams despues del div de return de la condicion style.bacgroundColor = 'red'.... ahi o estamos cambian que cuand no demuestre los nombres este en verde y cuando ya lo demuestre se ponga en rojo-----65

--- src/App.js
+++ src/App.js
@@ -85,12 +85,17 @@ togglePersonsHandler = () => {
        style.backgroundColor = 'red';
      }
 //Creando classes para diseno.
-     let classes  =['red', 'bold'].join(' ');
-
+     const classes  =[];
+     if (this.state.persons.length <= 1){
+       classes.push('red');
+     }
+     if (this.state.persons.legth <= 0){
+       classes.push('bold')
+     }
      return (
        <div className="App">
           <h1> Hi, I'm a React App </h1>
-          <p className={classes}> This is really working!!</p>
+          <p className={classes.join(' ')}> This is really working!!</p>
           <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
        </div>
